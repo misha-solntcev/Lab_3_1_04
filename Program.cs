d91092241a972c531f8666b8e9b48f6581ea903c
misha-solntcev <misha.solntcev@gmail.com>
2022-11-25 12:53:40 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 4. Дано целое число N (> 0). Если оно является степенью числа 3, 
+    то вывести True, если не является — вывести False.*/
+
+namespace Lab_3_1_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 9;
+            Console.WriteLine(N % 3 == 0);
+            Console.ReadKey();
+        }
+    }
+}
